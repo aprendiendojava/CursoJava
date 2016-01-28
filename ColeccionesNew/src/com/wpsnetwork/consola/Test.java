eb2069f4581553d25959cb911aa8aea5a35bfa96
Acruz00 <Usuario@PC-48>
2016-01-28 09:09:25 +0100

    Nuevo proyecto Collections

---
+++ ColeccionesNew/src/com/wpsnetwork/consola/Test.java
@@ -0,0 +1,18 @@
+package com.wpsnetwork.consola;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+	// OCA Oracle Certified Associate java SE 8 PROGRAMMER I -- 1Z0-808 -- Editorial Sybex
+	
+	// OCP Oracle Certified Professional java SE 8 PROGRAMMER II -- 1Z0-809 -- Editorial Sybex
+	
+	public static void main(String[] args) {
+		List<String> lista = new ArrayList<>();
+		
+		// Versión gráfica de java: AWT, SWING.
+	}
+
+}
