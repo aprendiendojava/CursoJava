75a4acb85439f4e624c357e770107be63c8a0aa2
Acruz00 <Usuario@PC-48>
2016-02-01 08:59:09 +0100

    subo (por primera vez) el proyecto 1

---
+++ Proyecto1/src/com/wpsnetwork/dao/entidades/Prestamo.java
@@ -0,0 +1,62 @@
+package com.wpsnetwork.dao.entidades;
+
+import java.util.Date;
+
+public class Prestamo {
+	private int id;
+	private Date fechaInicio;
+	private Date fechaFin;
+	private char devuelto;
+	private int libro;
+	private int persona;
+	public Prestamo() {
+		super();
+	}
+	public Prestamo(int id, Date fechaInicio, Date fechaFin, char devuelto, int libro, int persona) {
+		super();
+		this.id = id;
+		this.fechaInicio = fechaInicio;
+		this.fechaFin = fechaFin;
+		this.devuelto = devuelto;
+		this.libro = libro;
+		this.persona = persona;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+	public Date getFechaFin() {
+		return fechaFin;
+	}
+	public void setFechaFin(Date fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+	public char getDevuelto() {
+		return devuelto;
+	}
+	public void setDevuelto(char devuelto) {
+		this.devuelto = devuelto;
+	}
+	public int getLibro() {
+		return libro;
+	}
+	public void setLibro(int libro) {
+		this.libro = libro;
+	}
+	public int getPersona() {
+		return persona;
+	}
+	public void setPersona(int persona) {
+		this.persona = persona;
+	}
+	
+	
+}
---
+++ Proyecto1/src/com/wpsnetwork/dao/memoria/RepositorioLibrosMemoriaDao.java
@@ -0,0 +1,40 @@
+package com.wpsnetwork.dao.memoria;
+
+import java.util.List;
+
+import com.wpsnetwork.dao.entidades.Libro;
+import com.wpsnetwork.dao.interfaces.Dao;
+
+public class RepositorioLibrosMemoriaDao implements Dao<Libro>{
+
+	@Override
+	public Libro get(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(Libro elemento) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Libro elemento) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Libro elemento) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Libro> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
