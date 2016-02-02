a3fb4491c17448afff9b481f0781c558c9925963
acruz <acruz@antoniocr-pc.wpsnetwork.com>
2016-02-02 16:20:51 +0100

    antes de las clases de inglés. Creo los Dto (sin terminar)

--- Proyecto1/src/com/wpsnetwork/dao/memoria/RepositorioCategoriasLibrosMemoriaDao.java
+++ Proyecto1/src/com/wpsnetwork/dao/memoria/RepositorioCategoriasLibrosMemoriaDao.java
@@ -9,31 +9,26 @@ public class RepositorioCategoriasLibrosMemoriaDao implements Dao<CategoriaLibro
 
 	@Override
 	public CategoriaLibro get(int id) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void insert(CategoriaLibro elemento) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void update(CategoriaLibro elemento) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void delete(CategoriaLibro elemento) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public List<CategoriaLibro> getAll() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
---
+++ Proyecto1/src/com/wpsnetwork/dto/AutoresDto.java
@@ -0,0 +1,14 @@
+package com.wpsnetwork.dto;
+
+import com.wpsnetwork.dao.memoria.RepositorioAutoresMemoriaDao;
+import com.wpsnetwork.dto.entidades.Autor;
+
+public class AutoresDto {
+	private Autor autorDto;
+
+	public AutoresDto(RepositorioAutoresMemoriaDao repAutor) {
+		super();
+	}
+	
+	
+}
