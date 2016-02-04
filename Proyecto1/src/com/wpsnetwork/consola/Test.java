444b75ff6912abb9b8b3c491b0e8447f7a55689f
acruz <acruz@antoniocr-pc.wpsnetwork.com>
2016-02-04 16:47:47 +0100

    em

---
+++ Proyecto1/src/com/wpsnetwork/consola/Test.java
@@ -0,0 +1,45 @@
+package com.wpsnetwork.consola;
+
+import org.apache.log4j.Logger;
+
+public class Test {
+ 
+	// Porqué? Porque eso pone en "http://www.javatutoriales.com/2011/04/log4j-para-creacion-de-eventos-de-log.html"
+	private static Logger log = Logger.getLogger(Test.class);
+	
+	
+	public static void main(String[] args) {
+		/**
+		 * Ejemplo de internet 
+		 */
+		/**
+		Logger logger = Logger.getLogger(Test.class);
+		logger.trace("Ejemplo de traza");
+		try {
+			int a = 1/0;
+			System.out.println(a);
+		} catch (Exception e) {
+			logger.error(e);
+		}
+		*/
+		
+		
+		
+		/**
+		 * 
+		 * Ejemplo mio
+		 * 
+		 * Vamos a diferenciar los distintos tipos de traza/error fatal.
+		 * 
+		 * 
+		 */
+	    log.trace("mensaje de trace");
+	    log.debug("mensaje de debug");
+	    log.info("mensaje de info");
+	    log.warn("mensaje de warn");
+	    log.error("mensaje de error");
+	    log.fatal("mensaje de fatal");
+		
+		
+	}
+}
